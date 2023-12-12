1ad1cd762b2048b79ab1b352ca31706b8674165a
Do Tu Tai <tutai92@gmail.com>
2023-12-12 23:11:37 +0800

    commit code

--- analytics/config.py
+++ analytics/config.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db_username = os.environ["DB_USERNAME"]
 db_password = os.environ["DB_PASSWORD"]
-db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_host = os.environ.get("DB_HOST", "project03-postgresql")
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
